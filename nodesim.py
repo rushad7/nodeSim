f7e48cace68953ac9d7af728cb9b212fea49118b
rushad7 <rushadmehta16@gmail.com>
2020-12-15 22:32:12 +0530

    Added backProp() method to compute the gradients of the loss function. Modified updateNodeCoord() method to update node coordinates. Split forwardProp() fucntionality to updateBallCoord() method.

--- nodesim.py
+++ nodesim.py
@@ -6,6 +6,7 @@ class Network:
         
         self.ssLength = ssLength
         self.ssBreadth = ssBreadth
+        self.goalCoordinate = torch.tensor([self.ssLength, self.ssBreadth])
         self.numberOfNodes = numberOfNodes
         self.weights = torch.rand(4, numberOfNodes)
         self.nodeCoordinate = torch.rand(numberOfNodes, 2)*min(ssLength, ssBreadth)
@@ -38,31 +39,92 @@ class Network:
 
     def forwardProp(self):
         z = self.weights@torch.transpose(self.inputVector, 0, 1) 
-        a = min(self.ssLength, self.ssBreadth)*torch.sigmoid(z)
+        self.a = min(self.ssLength, self.ssBreadth)*torch.sigmoid(z)
 
-        point1 = torch.tensor([a[0], a[1]])
-        point2 = torch.tensor([a[2], a[3]])
+    def updateBallCoord(self):
+        point1 = torch.tensor([self.a[0], self.a[1]])
+        point2 = torch.tensor([self.a[2], self.a[3]])
 
         distVector1 = self.nodeToBallDist(ballCoordinate=point1)
         distVector2 = self.nodeToBallDist(ballCoordinate=point2)
         distVector = torch.cat((distVector1, distVector2))
-        minDist = torch.min(distVector)
+        minDist = torch.min(distVector1)
         minDistIndex = torch.where(distVector == minDist)[0].tolist()[0]
 
         if minDistIndex < len(distVector)/2:
             self.ballCoordinate = point1
+            self.a = point1
         else:
             self.ballCoordinate = point2
+            self.a = point2
 
-    '''
     def updateNodeCoord(self):
-        self.nodeToBallDist()
-    '''
+        distVector = self.nodeToBallDist()
+        minDist = torch.min(distVector)
+        minDistIndex = torch.where(distVector == minDist)[0].tolist()[0]
+        self.nodeCoordinate[minDistIndex] = self.ballCoordinate
+
+    def backProp(self):
+        
+        def gradJW(grad_JG, grad_GZ, grad_JW):
+            grad_JW = grad_JG * torch.transpose(grad_GZ, 0, 1) @ grad_JW
+            return  grad_JW
+        
+        def gradJG(axis):
+
+            if axis == "x":
+                grad_JG_num = self.goalCoordinate[0] - self.ballCoordinate[0]
+            else:
+                grad_JG_num = self.goalCoordinate[1] - self.ballCoordinate[1]
+            
+            xdiff_sqr = torch.pow(self.goalCoordinate[0] - self.ballCoordinate[0], 2)
+            ydiff_sqr = torch.pow(self.goalCoordinate[1] - self.ballCoordinate[1], 2)
+            grad_JG_den = torch.sqrt(xdiff_sqr + ydiff_sqr)
+
+            grad_JG = grad_JG_num/grad_JG_den
+            return grad_JG
+            
+        def gradGZ():
+            grad_GZ = self.a - torch.pow(self.a, 2)
+            grad_GZ = grad_GZ.unsqueeze(0)
+            return grad_GZ
+
+        def gradZW():
+            grad_ZW = self.inputVector
+            return grad_ZW
+
+        gradJWX = gradJW(gradJG(axis="x"), gradGZ(), gradZW())
+        gradJWY = gradJW(gradJG(axis="y"), gradGZ(), gradZW())
+        self.grad = torch.cat((gradJWX, gradJWY))
+
+    def computeCost(self):
+        self.cost = torch.sqrt(torch.sum((self.ballCoordinate.unsqueeze(0) - self.goalCoordinate.unsqueeze(0))**2, axis=1))
+
+    def gradientDescent(self, learing_rate):
+        self.weights = self.weights - learing_rate*self.grad
 
 net = Network(4, 3, 3)
+print("Weights")
+print(net.weights)
 net.computeInput()
+print("Ball Coordinates")
 print(net.ballCoordinate)
+print("Node Coordinates")
 print(net.nodeCoordinate)
+print("Cost")
+net.computeCost()
+print(net.cost)
 net.forwardProp()
+net.updateBallCoord()
+net.updateNodeCoord()
+print("Ball Coordinates")
 print(net.ballCoordinate)
+print("Node Coordinates")
 print(net.nodeCoordinate)
+net.backProp()
+net.gradientDescent(0.2)
+print("Weights")
+print(net.weights)
+print("Cost")
+net.computeCost()
+print(net.cost)
