00aed326a8f7e60c52e4720212ff37444937c729
rushad7 <rushadmehta16@gmail.com>
2020-12-22 02:01:27 +0530

    Modified __init__ method . Used coorMap dict for coordinate value retraction. Removed redundant methods: setLim, xCoord, yCoord, and update.

--- animate.py
+++ animate.py
@@ -1,50 +1,26 @@
-import torch
+import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 
 class Animation:
-    
-    def __init__(self, dims, nodeCoordinates, ballCoordinate):
-        self.dims = dims
-        self.nodeCoordinates = nodeCoordinates
-        self.balllCoordinate = ballCoordinate 
+
+    def __init__(self, coordMap, dims):
         self.fig, self.ax = plt.subplots()
-        self.xdata, self.ydata = [], []
-        self.ln, = plt.plot([], [], 'ro')
+        self.ax.set_xlim(0, dims[0])
+        self.ax.set_ylim(dims[1])
+        self.coordMap = coordMap
+        self.x = list(coordMap.keys())
+        self.y = list(coordMap.values())
+        self.line, = self.ax.plot(self.x, self.y, 'ro')
 
-    def setLim(self):
-        self.ax.set_xlim(0, self.dims[0])
-        self.ax.set_ylim(0, self.dims[1])
-        return self.ln,
+    def animate(self, i):
+        self.line.set_ydata(self.coordMap[i])
+        return self.line,
 
-    def xCoord(self):
-        nodeCoordX = self.nodeCoordinates[:, 0]
-        ballCoordX = self.balllCoordinate[0]
-        xVector = torch.cat((nodeCoordX, ballCoordX.unsqueeze(0)))
-        print(xVector)
-        return xVector.tolist()
-
-    def yCoord(self):
-        nodeCoordY = self.nodeCoordinates[:, 1]
-        ballCoordY = self.balllCoordinate[1]
-        yVector = torch.cat((nodeCoordY, ballCoordY.unsqueeze(0)))
-        print(yVector)
-        return yVector.tolist()
-
-    def update(self, frame):
-        print(self.xdata, self.ydata)
-        self.xdata.clear()
-        self.ydata.clear()
-        self.xdata.append(self.xCoord())
-        self.ydata.append(self.yCoord())
-        self.ln.set_data(self.xdata, self.ydata)
-        return self.ln,
+    def init(self):
+        self.line.set_ydata(np.ma.array(self.x, mask=True))
+        return self.line,
 
     def start(self):
-        _ = FuncAnimation(self.fig, self.update, init_func=self.setLim, blit=True)
+        _ = animation.FuncAnimation(self.fig, self.animate, self.x, init_func=self.init, interval=100, blit=True)
         plt.show()
-
-nodecoord = torch.randint(4, (4,2))
-ballcoord = 4*torch.rand([2])
-animation = Animation(dims=(4,4), nodeCoordinates=nodecoord, ballCoordinate=ballcoord)
-animation.start()
